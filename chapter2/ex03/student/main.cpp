e2b2c992fa95b226032861361fbc13fead7b36bb
Teddy Duong <130862402+Teddy5o4@users.noreply.github.com>
2025-08-28 21:26:15 +0000

    Testing Thu Aug 28 2025 21:26:15 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -1,16 +1,19 @@
 //include statement(s)
-
+#include <iostream>
 //using namespace statement
-
+using namespace std;
 
 int main()
 {
 
 //variable declaration
-
+int num1 = 125;
+int num2 = 28;
+int num3 = -25;
+int average = 0;
 
 //executable statements
+average = (num1 + num2 + num3) / 3;
 
-
-//return 0;
+return 0;
 }
