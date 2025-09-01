8ce13e60484bd241879ac970e2922c85ece51952
Teddy Duong <130862402+Teddy5o4@users.noreply.github.com>
2025-09-01 21:04:42 +0000

    Testing Mon Sep 01 2025 21:04:42 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex03/student/main.cpp
+++ chapter3/ex03/student/main.cpp
@@ -1,8 +1,26 @@
+// This program converts the weight of a person from kilograms
+// to pounds.
+
+// Date: 9/1/2025
 #include <iostream>
 
 using namespace std;
 
 int main() {
     // Write your main here
+    double kg = 0.0;
+    double ibs = 0.0;
+    const double CONVERSION = 2.2;
+
+    // Prompt user to enter weight in kilogram
+
+    cout << "Enter your weight in kilograms." << endl;
+    cin >> kg;
+
+    ibs = CONVERSION * kg;
+
+    // Display the person's weight in pounds
+
+    cout << "Your weight is " << ibs << " ibs" << endl;
     return 0;
 }
