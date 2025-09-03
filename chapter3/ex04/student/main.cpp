32ba690025b202f3c66da61af946824bc5e32111
Teddy Duong <130862402+Teddy5o4@users.noreply.github.com>
2025-09-03 23:29:38 +0000

    Testing Wed Sep 03 2025 23:29:38 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/main.cpp
+++ chapter3/ex04/student/main.cpp
@@ -17,7 +17,7 @@ int main()
     cin >> bagSize;
     cout << endl;
     
-    cout << "Enter the cost of the " << bagSize
+    cout << "Enter the cost in dollar of the " << bagSize
          << " pound fertilizer bag: ";
     cin >> cost;
     cout << endl;
@@ -28,9 +28,14 @@ int main()
     cout << endl;
     
     cout << "The cost of the fertilizer per pound is: $"
-         << bagSize / cost << endl;
+         << cost / bagSize << endl;
+
+    cout << endl;
     
     cout << "The cost of fertilizing per square foot is: $"
-         << area / cost << endl;
+         << cost / area << endl;
+
+    cout << endl;
+    
     return 0;
 }
